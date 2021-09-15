25c6477da8b0e795d25a1fff2292e39f1f31174e
Murilo Messias <murilo.messias@gmail.com>
2021-09-15 14:01:13 -0300

    Add Theme for Styled Components

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,13 @@
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      black: string
+      gray: string
+      lightBlack: string
+      primary: string
+      white: string
+    }
+  }
+}
+
+export {}
---
+++ src/resources/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  colors: {
+    black: '#1E293B',
+    gray: '#E4E5E7',
+    lightBlack: '#293445',
+    primary: '#1FC8E1',
+    white: '#FAFAFA',
+  },
+}
