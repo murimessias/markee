593f8bd20818233b36f10fb9c19c9d932e1af2cb
Murilo Messias <murilo.messias@gmail.com>
2021-09-16 14:54:53 -0300

    Add Input component

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,3 +1,4 @@
 import { Button } from './button/button'
+import { Input } from './input/input'
 
-export { Button }
+export { Button, Input }
---
+++ src/components/forms/input/input.tsx
@@ -0,0 +1,6 @@
+// Styles
+import * as S from './styles'
+
+const Input = () => <S.Wrapper type='text' defaultValue='Novo arquivo.md' />
+
+export { Input }
