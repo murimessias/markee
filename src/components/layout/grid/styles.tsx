7d83db4d31ba835b30ae83619766ab0039e0f9a8
Murilo Messias <murilo.messias@gmail.com>
2021-09-18 11:40:43 -0300

    Update Icons export and files

--- src/components/layout/grid/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/macro'
-
-export const Wrapper = styled.main`
-  display: grid;
-  grid:
-    'sidebar content' 1fr
-    / minmax(auto, 40rem) 1fr;
-  min-height: 100vh;
-`
